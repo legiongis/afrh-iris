4e25ad1f1c02442c459738e4c35c4d4bfdc7058c
Adam Cox <mr.adamcox@gmail.com>
2026-02-24 16:03:09 -0600

    fix paths to load data

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,17 +1,16 @@
 #! /usr/bin/bash
 
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Archaeological_Zone_2025-09-06_17-43-28.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Character_Area.json -ow overwrite
+python manage.py packages -o import_business_data -s ./pkg/business_data/local/Character_Areas.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Master_Plan_Zone_2025-09-14_10-56-59.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Historic_Area_2025-09-07_11-38-41.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Person.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Management_Activity_2025-10-23_13-12-41.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Management_Activity_2025-09-07_09-09-45.json -ow overwrite
+# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Management_Activity_2025-10-23_13-12-41.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Information_Resource.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Inventory_Resource_2025-09-06_10-27-01.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_CFA_2025-10-23_12-35-19.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NCPC_2025-10-23_12-35-03.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NEPA_2025-10-23_12-34-33.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_Section_106_2025-10-23_12-34-10.json -ow overwrite
+# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_CFA_2025-10-23_12-35-19.json -ow overwrite
+# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NCPC_2025-10-23_12-35-03.json -ow overwrite
+# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NEPA_2025-10-23_12-34-33.json -ow overwrite
+# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_Section_106_2025-10-23_12-34-10.json -ow overwrite
 
 python manage.py es reindex_database
