718df14cc5d274c6fb418514197dee068c180573
Adam Cox <mr.adamcox@gmail.com>
2026-03-25 14:38:47 -0500

    update versions of migrated data to load

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -4,13 +4,8 @@ python manage.py packages -o import_business_data -s ./pkg/business_data/local/A
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Character_Areas.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Master_Plan_Zone_2025-09-14_10-56-59.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Historic_Area_2025-09-07_11-38-41.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Person.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Management_Activity_2025-10-23_13-12-41.json -ow overwrite
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Information_Resource.json -ow overwrite
+python manage.py packages -o import_business_data -s ./pkg/business_data/local/Person-Organization_2026-03-25_14-37-08.json -ow overwrite
+python manage.py packages -o import_business_data -s ./pkg/business_data/local/Information_Resource_2026-03-25_14-37-27.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Inventory_Resource_2025-09-06_10-27-01.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_CFA_2025-10-23_12-35-19.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NCPC_2025-10-23_12-35-03.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_NEPA_2025-10-23_12-34-33.json -ow overwrite
-# python manage.py packages -o import_business_data -s ./pkg/business_data/local/Review-_Section_106_2025-10-23_12-34-10.json -ow overwrite
 
 python manage.py es reindex_database
