2773271c4c6cfc48fe433d8fb127fdc835217f74
Adam Cox <mr.adamcox@gmail.com>
2026-03-27 16:22:49 -0500

    load newer file

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-python manage.py packages -o import_business_data -s ./pkg/business_data/local/Archaeological_Zone_2025-09-06_17-43-28.json -ow overwrite
+python manage.py packages -o import_business_data -s ./pkg/business_data/local/Archaeological_Zone_2026-03-27_16-03-51.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Character_Areas.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Master_Plan_Zone_2025-09-14_10-56-59.json -ow overwrite
 python manage.py packages -o import_business_data -s ./pkg/business_data/local/Historic_Area_2025-09-07_11-38-41.json -ow overwrite
